2524f9e263081f26fdec58d0f01bdab97f8c417f
Stanley <ticktick58@gmail.com>
2015-04-01 16:55:10 -0700

    uploading files

---
+++ Assets/Scripts/Unit/UnitAI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitAI : MonoBehaviour {
+	public float attackRange;
+	public float attackInterval;
+	UnitAttributes attr;
+	NavMeshAI ai;
+	public float enemyDistance;
+	public GameObject enemy;
+	bool attacking;
+	public bool hasEnemy;
+
+	// Use this for initialization
+	void Start () {
+		attr = GetComponent<UnitAttributes> () as UnitAttributes;
+		ai = GetComponent<NavMeshAI> () as NavMeshAI;
+		attacking = false;
+		hasEnemy = true;
+		attackInterval = 3.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasEnemy) {
+			//attack if it has an enemy
+			enemyDistance = Vector3.Distance (enemy.transform.position, transform.position);
+			if (enemyDistance <= attackRange) {
+				if (!attacking) {
+					Invoke ("ApplyDamage", attackInterval);	//do damage every attackInt seconds
+					attacking = true;
+				}
+				if(enemy.GetComponent<UnitAttributes>().GetUnitHP() <= 0) {
+					Destroy(enemy);
+					hasEnemy = false;
+				}
+			}
+		} else {
+			//search for an enemy
+			//enemy = 
+			//
+		}
+	}
+
+	public void SetEnemy(GameObject enemy) {
+		this.enemy = enemy;
+		ai.SetTarget (enemy);
+		hasEnemy = true;
+	}
+
+	public void ApplyDamage() {
+		//call the method on the enemy to take damage
+		if (enemy != null) {
+			enemy.SendMessage ("TakeDamage", attr.GetUnitAttack (), SendMessageOptions.RequireReceiver);
+		}
+		attacking = false;
+
+	}
+
+}
---
+++ Assets/Scripts/Unit/UnitAttributes.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitAttributes : MonoBehaviour {
+	public int unitHP;
+	public int unitAttack;
+//	public Transform target;
+//	public GameObject enemy;
+//	public bool hasEnemy;
+	bool attacking;
+
+	// Use this for initialization
+	void Start () {
+		this.unitHP = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (unitHP <= 0) {
+			Death();
+		}
+	}
+
+	void TakeDamage(int damage) {
+		unitHP -= damage;
+	}
+
+	void Death() {
+		Destroy (this);
+	}
+
+	public int GetUnitHP() {
+		return this.unitHP;
+	}
+
+	public void SetUnitHP(int hp) {
+		this.unitHP = hp;
+	}
+
+	public int GetUnitAttack() {
+		return this.unitAttack;
+	}
+
+	public void SetUnitAttack(int att) {
+		this.unitAttack = att;
+	}
+}
